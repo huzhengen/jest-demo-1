4e78d7aeeca038c36c9e2471be3dc4e7c3746c47
hukai <huzhengen@163.com>
2020-06-02 17:14:59 +0800

    leetcode1

--- leetcode1.js
+++ leetcode1.js
@@ -1,17 +1,60 @@
-/**
- * @param {number[]} nums
- * @param {number} target
- * @return {number[]}
- */
-export const twoSum = function (nums, target) {
+export const twoSum1 = function (nums, target) {
   let arr = [];
   for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      if (nums[i] + nums[j] === target) {
-        arr.push(i)
-        arr.push(j)
-        return arr;
+      for (let j = i + 1; j < nums.length; j++) {
+          if (nums[i] + nums[j] === target) {
+              arr.push(i)
+              arr.push(j)
+              return arr;
+          }
       }
-    }
+  }
+};
+
+export const twoSum2 = function (nums, target) {
+  let obj = {};
+  for (let i = 0; i < nums.length; i++) {
+      obj[nums[i]] = i
+  }
+
+  for (let i = 0; i < nums.length; i++) {
+      if (obj.hasOwnProperty(target - nums[i]) && obj[target - nums[i]] !== i) {
+          return [i, obj[target - nums[i]]]
+      }
+  }
+};
+
+export const twoSum3 = function (nums, target) {
+  let map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+      map.set(nums[i], i);
+  }
+
+  for (let i = 0; i < nums.length; i++) {
+      if (map.has(target - nums[i]) && map.get(target - nums[i]) !== i) {
+          return [i, map.get(target - nums[i])]
+      }
+  }
+};
+
+
+
+export const twoSum4 = function (nums, target) {
+  let obj = {}
+  for(let i=0; i<nums.length; i++){
+      if(obj.hasOwnProperty(target - nums[i])){
+          return [obj[target-nums[i]], i]
+      }
+      obj[nums[i]] = i;
+  }
+};
+
+export const twoSum5 = function (nums, target) {
+  let map = new Map();
+  for(let i=0; i<nums.length; i++){
+      if(map.has(target - nums[i])){
+          return [map.get(target-nums[i]), i]
+      }
+      map.set(nums[i], i)
   }
 };
--- leetcode1.test.js
+++ leetcode1.test.js
@@ -1,5 +1,17 @@
-const { twoSum } = require('./leetcode1.js')
+const { twoSum1,twoSum2,twoSum3,twoSum4,twoSum5 } = require('./leetcode1.js')
 
 test('success', () => {
-  expect(twoSum([2, 7, 11, 15], 9)).toEqual([0, 1])
+  expect(twoSum1([2, 7, 11, 15], 9)).toEqual([0, 1])
+  expect(twoSum2([2, 7, 11, 15], 9)).toEqual([0, 1])
+  expect(twoSum3([2, 7, 11, 15], 9)).toEqual([0, 1])
+  expect(twoSum4([2, 7, 11, 15], 9)).toEqual([0, 1])
+  expect(twoSum5([2, 7, 11, 15], 9)).toEqual([0, 1])
+
+  expect(twoSum1([4, 4, 11, 15], 8)).toEqual([0, 1])
+  expect(twoSum2([4, 4, 11, 15], 8)).toEqual([0, 1])
+  expect(twoSum3([4, 4, 11, 15], 8)).toEqual([0, 1])
+  expect(twoSum4([4, 4, 11, 15], 8)).toEqual([0, 1])
+  expect(twoSum5([4, 4, 11, 15], 8)).toEqual([0, 1])
+
+  // expect(twoSum5([4, 4, 11, 15], 8)).toEqual([10, 1]) // error
 })
